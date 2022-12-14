856d9a5ce0dc7c2b739ceb10235ce00e655a04bb
zhongxuan_sun <zhongxuan_sun@asiainfo-sec.com>
2022-12-14 09:57:10 +0800

    增加第六章代码

--- booktool/src/c06/my-06.asm
+++ booktool/src/c06/my-06.asm
@@ -23,7 +23,53 @@ start:
     cld                             ;设置拷贝方向为正向拷贝（di寄存器）
     rep movsw
 
-    ; 设置死循环
+; 计算number标号对应汇编地址，并将其分解成个十百千万
+    ; 保存number标号地址到ax寄存器中，因为div指令的被除数需要放在dx:ax中
+    mov ax, number
+    ; 后面要保存商到number中，这里要记一下number的地址
+    mov bx, ax
+    ; 保存除数
+    mov si, 10
+    ; 设置循环次数cx寄存器，因为loop每次会把cx寄存器值减1
+    ; 我们正好是个十百千万
+    mov cx, 5
+digit:
+    ; 每次都要把dx清零，因为是32位除16位
+    xor dx, dx
+    div si
+    ; 保存余数到number中
+    ; 余数保存在dx寄存器中，因为肯定是小于10，所以取低8位就够了
+    mov [bx], dl
+    ; bx加1
+    inc bx
+    ; 循环计算
+    loop digit
+
+; 到此为止，number中已经按照个十百千万保存了number处的地址
+; 下面需要做的是把number中的值搬运到显存中
+; 因为number中保存的是数字，所以要将其转成ASCII码，每个数字加0x30
+    mov bx, number
+    mov si, 0x4
+show:
+    ; 拷贝number中的值到al寄存器中，因为我们要用ax寄存器临时保存
+    ; 放到显存中的值
+    mov al, [bx + si]
+    ; 将数字转成ASCII码
+    add al, 0x30
+    ; 设置显示属性为0x04
+    mov ah, 0x04
+    ; 搬运ax寄存器中值到显存中，es中保存显存的基地址,es:di正好在":"后面
+    mov [es:di], ax
+    ; 显存index + 2，因为高位是显示属性，低位是ASCII码
+    add di, 2
+    ; si减一，继续拷贝后面的值
+    dec si
+    ; 当si减到-1时，sf标志位被设置，继续执行jns show下面的指令
+    jns show
+    ; 设置最后的"D"字符
+    mov word [es:di],0x0744
+
+    ; 死循环
     jmp near $
 
     ; padding 补齐512字节
