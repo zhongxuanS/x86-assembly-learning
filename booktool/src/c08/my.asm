dda1a51e8d43ae7e22e668d3b424f26b7bd3fcb4
zhongxuan <zhongxuan_sun@outlook.com>
2023-01-04 22:06:21 +0800

    add init ds logic

--- booktool/src/c08/my.asm
+++ booktool/src/c08/my.asm
@@ -1,4 +1,36 @@
+    ; 用户程序起始扇号100，也可以声明成
+    ; app_lba_start db 100，这样的话会占用内存和磁盘空间
+    app_lba_start equ 100
+
+
+; 引导程序的作用：
+; 1. 从100扇号读取用户程序到0x10000内存处，使用0x10000是因为8086只能管理1MB内存，
+;    0x0 0000~0xf ffff
+;    0x0000~0x7c00~0xffff被MBR/BIOS占用，高位还有被IO映射占用，只有中间一截是可以被利用的，
+;    0x10000肯定是空闲，可以被占用
 section mbr align=16 vstart=0x7c00
+    ; 用户程序内存映射基地址
+    phy_base dd 0x10000
+
+    ; 计算用户程序内存的段地址，0x1000，保存到ds和es寄存器中
+    ; 这是因read_hard_disk_0函数映射磁盘数据到内存中的时候，
+    ; 使用ds:bx来操作内存。所以要映射到ds中
+    mov ax, [cs:phy_base]
+    mov dx, [cs:phy_base+0x02]
+    mov bx, 16
+    div bx
+    ; 商在ax中
+    mov ds, ax
+    mov es, ax 
+
+
+    ; 读取扇号为100的磁盘
+    xor di, di
+    mov si, app_lba_start
+    xor bx, bx
+    call read_hard_disk_0
+
+
 
 ; 扇区编号: DI:SI（高16位，低16位）
 ; 默认只读取1个扇区
@@ -53,7 +85,8 @@ read_hard_disk_0:
 
     mov cx, 256 ; 读取256字
     mov dx, 0x1f0
-.readw
+
+.readw:
     in ax, dx
     mov [bx], ax
     add bx, 2
